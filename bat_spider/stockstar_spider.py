a04d51c8e57ded50da8135e29ff13c5e470fe724
zhu <1027719545@qq.com>
2019-11-07 22:48:36 +0800

    提取股票信息

--- bat_spider/stockstar_spider.py
+++ bat_spider/stockstar_spider.py
@@ -19,15 +19,19 @@ from fake_useragent import UserAgent
 from requests import RequestException
 from bs4 import BeautifulSoup
 
+
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 # 禁用安全请求警告
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 def request_url(url, url_decode="utf8"):
     sess = requests.session()
-    ua = UserAgent()
+    ua = UserAgent(verify_ssl=False)
     headers = {"User-Agent": ua.random}
     try:
         response = sess.get(url=url, headers=headers, verify=False, timeout=20)
@@ -38,12 +42,38 @@ def request_url(url, url_decode="utf8"):
         print('请求地址出错', url)
 
 
+def request_webdriver(url):
+    chrome_options = Options()  # 创建配置对象
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    #  驱动的路径
+    path = r"F:\python_tool\chromedriver.exe"
+    driver = webdriver.Chrome(executable_path=path, chrome_options=chrome_options)
+    driver.get(url)
+    page_source = driver.page_source
+
+    print(page_source)
+    return page_source
+    # soup = BeautifulSoup(data, 'lxml', from_encoding="utf8")
+    # print(soup)
+
+
 def parse_info(data):
-    soup = BeautifulSoup(data, 'lxml', from_encoding="utf8")
-    spanlist = soup.find_all("span", classmethod="contentpile__content__wrapper__item__info__box__jobname__title")
-    print(spanlist)
+    soup = BeautifulSoup(data, "lxml", from_encoding="gb2312")
+    print(soup)
+    mytable = soup.find_all(id="datalist")
+    print(mytable)
+    # mytable[0] 第一个表格
+    for line in mytable[0].select("tr"):
+        for td in line.select("td"):
+            # print(td.get_text())
+            # print(td.string)
+
+
 
 
 if __name__ == '__main__':
-    resp = request_url(url="https://sou.zhaopin.com/?jl=765&kw=python&kt=3")
+    resp = request_webdriver(url="http://quote.stockstar.com/fund/stock.shtml")
+    # resp = request_url(url=r"http://quote.stockstar.com/fund/stock.shtml", url_decode="gb2312")
     parse_info(resp)
+
