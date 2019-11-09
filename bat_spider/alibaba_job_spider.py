ce6d81771889eb61749febd0fe8798f70c88851b
zhu <1027719545@qq.com>
2019-11-09 22:55:40 +0800

    添加阿里巴巴岗位爬虫测试脚本

--- bat_spider/alibaba_job_spider.py
+++ bat_spider/alibaba_job_spider.py
@@ -14,3 +14,42 @@
 # Contributors:
 #
 # ------------------------------------------------------------------------------
+import time
+import requests
+from fake_useragent import UserAgent
+from requests import RequestException
+from selenium import webdriver
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# 禁用安全请求警告
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def request_url(url, url_decode="utf8"):
+    sess = requests.session()
+    ua = UserAgent()
+    headers = {"User-Agent": ua.random}
+    try:
+        response = sess.get(url=url, headers=headers, verify=False, timeout=20)
+        if response.status_code == 200:
+            return response.content.decode(url_decode, errors="ignore")
+    except RequestException as e:
+        print(e)
+        print('请求地址出错', url)
+
+
+def get_alibaba_page(url):
+    driver = webdriver.Chrome()
+    driver.get(url)
+    page_source = driver.page_source
+    time.sleep(5)
+    next_elem = driver.find_element_by_xpath('//div[@class="pagination"]//li[last()]//a')
+    next_elem.click()
+    time.sleep(20)
+    print(page_source)
+
+
+
+if __name__ == '__main__':
+    get_alibaba_page(url="https://job.alibaba.com/zhaopin/positionList.htm?spm=a2obv.11410899.0.0.55ef6c614l92f9")
