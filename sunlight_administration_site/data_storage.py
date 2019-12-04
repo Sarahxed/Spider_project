6c80f0db0b0302b671d660e77ebf8855f35a9379
zhu <1027719545@qq.com>
2019-12-04 23:08:52 +0800

    添加数据库建表和插入数据脚本

--- sunlight_administration_site/data_storage.py
+++ sunlight_administration_site/data_storage.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# !/usr/bin/env python
-#------------------------------------------------------------------------------
 # Name:  data_storage
 # Purpose: 
 # 
@@ -14,3 +10,42 @@
 # Contributors:
 #
 # ------------------------------------------------------------------------------
+from sqlalchemy import Column, Table, MetaData
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+from sunlight_administration_site.config import MYSQL_URL
+
+engine = create_engine(MYSQL_URL)  # 定义引擎
+metadata = MetaData(bind=engine)  # 绑定元信息
+
+Base = declarative_base()
+# Base.metadata.create_all(engine)
+
+
+class Sunlight(Base):
+    __tablename__ = "complain_table"
+    id = Column(Integer, primary_key=True)
+    url = Column(String(300), comment="链接")
+    record = Column(Integer, comment="编号")
+    title = Column(String(200), comment="主题")
+    status = Column(String(20), comment="状态")
+    netizen = Column(String(60), comment="网名")
+    time = Column(String(60), comment="时间")
+
+
+class Spider(object):
+    def __init__(self):
+        self.table = Table("complain_table", metadata, autoload=True)
+
+    def insert(self, url, record, title, status, netizen, time):
+        self.table.insert().values(url=url, record=record, title=title, status=status, netizen=netizen, time=time).execute()
+
+
+if __name__ == '__main__':
+    sun = Sunlight()
+#     # 创建映射数据表(如果不存在)
+    Base.metadata.create_all(engine)
+
